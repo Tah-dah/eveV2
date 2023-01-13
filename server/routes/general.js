c1c0e57471a427f6f3ed14f3462bc47cc8f37c34
tah-dah <markcfranz13@gmail.com>
2023-01-13 15:48:25 -0600

    route user id and db stats, implement passport.js

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { getUser, getDashboardStats } from "../controllers/general.js";
+
 
 const router = express.Router();
+
+router.get("/dashboard", getDashboardStats);
 
 export default router;
